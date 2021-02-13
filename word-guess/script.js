f55aa9809d98eeb7b61b9bd1e25326d9f8eb26d8
Marina Russ <marina@cameystudios.com>
2021-02-13 13:58:40 -0500

    created skeleton structure -rudimentary HTML coded -styles.css created -list of functions entered into script.js

---
+++ word-guess/script.js
@@ -0,0 +1,42 @@
+
+// Array of words the user will guess
+var words = [];
+
+// init fxn called when page is loaded
+function init() { };
+
+
+// startGame called when the start button is clicked
+function startGame() { };
+// you need to render the # of blanks and timer
+function startTimer() { };
+function renderBlanks() { };
+
+function checkWin() { 
+//* when all blanks are !=== hidden, show a game win screen ??
+};
+
+function checkLetters(letter)
+
+// screen to tell user the result
+function winGame() { };
+// when all blanks are !=== hidden, show a game win screen ??
+function loseGame() { 
+// if time runs out and any blanks are === hidden, show a game loss screen ??
+};
+
+// local server storage
+function setWins() { };
+function setLosses() { };
+function getWins() { };
+function getLosses() { };
+
+// event listener to listen for keydown event
+document.addEventListener("keydown", function(event) { });
+//* TODO: include an eventListener to reset timer when start button is clicked
+
+//*******************************************/
+
+
+
+
